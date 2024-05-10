3232798084cd9c36c93b004e44ed2c5af9dc52a5
pedro.paulo.espindola2710@gmail.com <131112587+EspindolaPedro@users.noreply.github.com>
2024-05-09 20:30:24 -0400

    introducao

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" >
     <title>Bikcraft - Bicicletas Elétricas</title>
     <link rel="stylesheet" href="src\css\style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 </head>
 <body>
     <header id="header-bg">          
@@ -24,12 +27,15 @@
     </header>
 
     <main class="introducao-bg">
-        <div class="introducao">
-            <h1>Bicicletas feitas sob medida.</h1>
-            <p>
-            Bicicletas elétricas de alta precisão e qualidade, feitas sob medida para o cliente. Explore o mundo na sua velocidade com a Bikcraft.
-            </p>
-            <img href="src\img\fotos\introducao.jpg" alt="Bicicleta elétrica preta"></img>
+        <div class="introducao container">
+            <div class="introducao-conteudo">
+                <h1>Bicicletas feitas sob medida<span class="ponto">.</span></h1>
+                <p>
+                Bicicletas elétricas de alta precisão e qualidade, feitas sob medida para o cliente. Explore o mundo na sua velocidade com a Bikcraft.
+                </p>
+                <a href="./bicicletas.html">Escolha a sua</a>
+            </div>           
+                <img src="src\img\fotos\introducao.jpg" alt="Bicicleta elétrica preta"></img>         
         </div>
     </main>
 
